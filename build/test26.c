86b3e47d7465d4d86dd383d15aa0e86a1a8fc7ce
zxy230 <147614548+zxy230@users.noreply.github.com>
2025-11-16 21:46:03 +0800

    Delete build directory

--- build/test26.c
+++
@@ -1,6 +0,0 @@
-int main() {
-    int c = 5;
-    int result = -(c + -1);
-    int result2 = --(c + -1);
-    return result;
-}
--- build/test_multi_vars.c
+++
@@ -1,5 +0,0 @@
-int main() {
-    int x = 1, y = 2;
-    int a = 3, b = 4, c = 5;
-    return x + y + a + b + c;
-}
